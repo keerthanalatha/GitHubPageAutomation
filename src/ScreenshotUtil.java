eba54eb3b41a0a6a4caaec81701bd2ce55c62716
keerthanalatha <keerthanalatha@users.noreply.github.com>
2025-02-19 19:39:22 +0530

    Update ScreenshotUtil.java

--- src/ScreenshotUtil.java
+++ src/ScreenshotUtil.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.io.FileHandler;
 import java.io.File;
 import java.io.IOException;
 import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
 
 public class ScreenshotUtil {
-    public static String captureScreenshot(WebDriver driver, String screenshotName) {
+    public static String captureScreenshot(WebDriver driver, String screenshotName) throws IOException{
         // File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         // String path = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
         // File dest = new File(path);

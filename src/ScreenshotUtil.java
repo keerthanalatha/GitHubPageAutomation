a9c65219118252b6281bf27163c80662b2ae68ee
keerthanalatha <keerthanalatha@users.noreply.github.com>
2025-02-19 19:31:37 +0530

    Update ScreenshotUtil.java

--- src/ScreenshotUtil.java
+++ src/ScreenshotUtil.java
@@ -7,14 +7,21 @@ import org.openqa.selenium.WebDriver;
 
 public class ScreenshotUtil {
     public static String captureScreenshot(WebDriver driver, String screenshotName) {
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String path = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
-        File dest = new File(path);
-        try {
-            FileHandler.copy(src, dest);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return path;
+        // File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // String path = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
+        // File dest = new File(path);
+        // try {
+        //     FileHandler.copy(src, dest);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
+        // return path;
+
+
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String relativePath = "screenshots/" + screenshotName + ".png";
+        String destinationPath = "test-output/" + relativePath;
+        FileUtils.copyFile(srcFile, new File(destinationPath));
+        return relativePath; // Return relative path for ExtentReports
     }
 }
